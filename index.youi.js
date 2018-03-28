837fa3a1d292c31a3a89e6e518027004b48e2890
Erik Uggeldahl <Erik.Uggeldahl@youilabs.com>
2018-03-28 16:02:57 -0400

    Initial PDP implementation
    Includes Geoff's first pass at the PDP.

--- index.youi.js
+++ index.youi.js
@@ -13,7 +13,8 @@ import {
   Composition,
   ImageRef,
   TextRef,
-  TimelineRef
+  TimelineRef,
+  Fragment
 } from 'react-native-youi';
 
 export default class YiReactApp extends Component {
@@ -21,10 +22,54 @@ export default class YiReactApp extends Component {
   constructor() {
     super();
     this.state = {
+      details: {},
+      selected: 0,
     };
   }
 
+  componentDidMount() {
+    this.requestPopularMoviesAsync((results) => {
+      let id = results[1].id;
+
+      this.requestMovieDetailsAsync(id, (asset) => {
+        this.setState({
+          selected: id,
+          details: {
+            ...this.state.details,
+            [id]: asset,
+          },
+        });
+
+        this.inTimeline.play();
+      });
+    });
+  }
+
+  requestPopularMoviesAsync = (callback) => {
+    return fetch("https://api.themoviedb.org/3/discover/movie?api_key=7f5e61b6cef8643d2442344b45842192")
+      .then((response) => response.json())
+      .then((responseJson) => {
+        callback(responseJson.results);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  requestMovieDetailsAsync = (assetID, callback) => {
+    return fetch("https://api.themoviedb.org/3/movie/" + assetID + "?api_key=7f5e61b6cef8643d2442344b45842192&append_to_response=releases,credits")
+      .then((response) => response.json())
+      .then((responseJson) => {
+        callback(responseJson);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   render() {
+    let metadata = this.state.details[this.state.selected] ? <Metadata asset={this.state.details[this.state.selected]} /> : null;
+
     return (
       <View
         style={styles.container}
@@ -32,10 +77,58 @@ export default class YiReactApp extends Component {
         <Composition
           source="PDP_Main"
         >
+          <TimelineRef
+            name="In"
+            onLoad={(timeline) => {
+              this.inTimeline = timeline;
+            }}
+          />
+          {metadata}
         </Composition>
       </View>
     );
   }
+}
+
+export function Metadata(props) {
+  let rating = props.asset.releases.countries.filter((release) => release["iso_3166_1"] === "US")[0].certification;
+  let director = props.asset.credits.crew.filter((member) => member["job"] === "Director")[0].name;
+  let stars = props.asset.credits.cast.slice(0, 3).map((member) => member["name"]).join(", ");
+  let posterSource = "https://image.tmdb.org/t/p/w500" + props.asset.poster_path;
+  let backdropSource = "https://image.tmdb.org/t/p/w1280" + props.asset.backdrop_path;
+
+  return (
+    <Fragment>
+      <TextRef
+        name="Title-Text"
+        text={props.asset.title}
+      />
+      <TextRef
+        name="Details-Text"
+        text={props.asset.release_date.split("-")[0] + (rating ? " | Rated " + rating : "") + " | " + props.asset.runtime}
+      />
+      <TextRef
+        name="Director"
+        text={director}
+      />
+      <TextRef
+        name="Stars"
+        text={stars}
+      />
+      <TextRef
+        name="Body-Text"
+        text={props.asset.overview}
+      />
+      <ImageRef
+        name="Image-2x3"
+        source={{ uri: posterSource }}
+      />
+      <ImageRef
+        name="Image-16x9"
+        source={{ uri: posterSource }}
+      />
+    </Fragment>
+  );
 }
 
 const styles = StyleSheet.create({
