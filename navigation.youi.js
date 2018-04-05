ab488dd2980148c640f31c6a68c7445d668970ef
Matan Bareket <matan.bareket@youi.tv>
2018-04-05 11:58:43 -0400

    Add basic navigation controller

---
+++ navigation.youi.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Navigation {
+  constructor() {
+
+  }
+  static stack = []
+
+  static onScreenChanged = () => {}
+
+  static addScreen(screen) {
+    this.stack.push(screen)
+    this.onScreenChanged(screen);
+  }
+
+  static popScreen() {
+    this.stack.pop()
+    this.onScreenChanged(this.getCurrentScreen())
+    return this.getCurrentScreen()
+  }
+
+  static getCurrentScreen() {
+    return this.stack[this.stack.length-1]
+  }
+}
+
+Navigation.stack = []
+
+export default Navigation
