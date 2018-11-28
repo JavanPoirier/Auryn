481488226befe94242c3222c20c3a2e4006e9165
matanbareketyoui <matan.bareket@youi.tv>
2018-11-28 11:48:05 -0500

    Remove old components

--- components/index.js
+++ components/index.js
@@ -1,17 +1,15 @@
-import Button from './button.youi';
 import ListItem from './listitem.youi';
 import Video from './video.youi';
-import Scrubber from './scrubber.youi';
 import Timeline from './timeline.youi';
 import DiscoverContainer from './discoverContainer.youi';
+import ToggleButton from './toggleButton.youi';
 import ToggleGroup from './toggleGroup.youi';
 
 export {
-  Button,
   ListItem,
-  Scrubber,
   Timeline,
   DiscoverContainer,
+  ToggleButton,
   ToggleGroup,
   Video,
 };
