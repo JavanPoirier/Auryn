f5f4bb3393da2eba5726d0f104de74b596176ae1
matanbareketyoui <matan.bareket@youi.tv>
2018-12-12 14:27:18 -0500

    Fix error when no formats are returned from youtube. (law and order)

--- reducers/youtubeReducer.js
+++ reducers/youtubeReducer.js
@@ -1,8 +1,10 @@
+const defaultVideoSource = {
+  uri: 'http://www.streambox.fr/playlists/x31jrg1/x31jrg1.m3u8',
+  type: 'HLS',
+};
+
 export default function youtubeReducer(state = { // eslint-disable-line max-lines-per-function
-  videoSource: {
-    uri: 'http://www.streambox.fr/playlists/x31jrg1/x31jrg1.m3u8',
-    type: 'HLS',
-  },
+  videoSource: defaultVideoSource,
   fetching: false,
   fetched: false,
   error: null,
@@ -14,8 +16,9 @@ export default function youtubeReducer(state = { // eslint-disable-line max-line
         fetching: true,
       };
     case 'YOUTUBE_VIDEO_FULFILLED': {
-      const format = action.payload.formats
-        .find(fmt => fmt.type.indexOf('mp4') > 0 && fmt.quality === 'hd720');
+      const format = action.payload.formats ?
+        action.payload.formats.find(fmt => fmt.type.indexOf('mp4') > 0 && fmt.quality === 'hd720')
+        : null;
 
       if (format) {
         return {
@@ -32,6 +35,7 @@ export default function youtubeReducer(state = { // eslint-disable-line max-line
       // No viable format found
       return {
           ...state,
+          videoSource: defaultVideoSource,
           fetching: false,
           fetched: true,
         };
