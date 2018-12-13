20627d55c19a7b80fe710ffe7c86e89c2de0fc6f
matanbareketyoui <matan.bareket@youi.tv>
2018-12-13 17:07:12 -0500

    CES: Add onFocus handler to AdListItem

--- ces2018/adListitem.youi.js
+++ ces2018/adListitem.youi.js
@@ -3,6 +3,11 @@ import { Composition, ButtonRef } from '@youi/react-native-youi';
 import { NavigationActions } from 'react-navigation';
 
 export default class AdListItem extends PureComponent {
+  static defaultProps = {
+    onFocus: () => {},
+    onPress: () => {},
+  }
+
   constructor(props) {
     super(props);
   }
@@ -20,6 +25,7 @@ export default class AdListItem extends PureComponent {
       <Composition source="Auryn_Container-Btn-Ad-Small" loadSync={true}>
         <ButtonRef
           focusable={this.props.focusable}
+          onFocus={() => this.props.onFocus(this.ref)}
           ref={ref => this.ref = ref}
           onPress={() => this.props.onPress('Ad', 'Ad', this.ref)}
           name="Btn-Backdrop-Ad"
