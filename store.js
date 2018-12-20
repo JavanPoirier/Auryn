bf3ad58650fbfa6ca6ecd48cd1b8161d5421cec3
matanbareketyoui <matan.bareket@youi.tv>
2018-12-20 15:49:53 -0500

    Remove redux-logger
    redux-logger creates a huge slowdown even in release mode

--- store.js
+++ store.js
@@ -1,11 +1,10 @@
 import { applyMiddleware, createStore } from 'redux';
-import logger from 'redux-logger';
 import promise from 'redux-promise-middleware';
 import createDebounce from 'redux-debounced';
 import thunk from 'redux-thunk';
 
 import reducer from './reducers';
 
-const middleware = applyMiddleware(createDebounce(), thunk, logger, promise());
+const middleware = applyMiddleware(createDebounce(), thunk, promise());
 
 export default createStore(reducer, middleware);
