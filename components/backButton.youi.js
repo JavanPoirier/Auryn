e2ef530bfeb530fd588e390fca8903ff905a31ea
matanbareketyoui <matan.bareket@youi.tv>
2018-12-21 21:15:06 -0500

    Add proptypes to backbutton

--- components/backButton.youi.js
+++ components/backButton.youi.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import { ButtonRef } from '@youi/react-native-youi';
+import PropTypes from 'prop-types';
 
 export default class BackButton extends PureComponent {
   constructor(props) {
@@ -15,3 +16,8 @@ export default class BackButton extends PureComponent {
       focusable={this.props.focusable && !this.hasHardwareBackButton}
     />
 }
+
+BackButton.propTypes = {
+  focusable: PropTypes.bool,
+};
+
