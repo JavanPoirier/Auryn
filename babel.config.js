e7b0d7a267a506492b8efd3ccfc4a12d10ce15b1
Erik Uggeldahl <erikuggeldahl@gmail.com>
2020-08-20 13:49:27 -0700

    Removed offending Babel config causing errors

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
-    ['@babel/plugin-transform-typescript', { allowDeclareFields: true }],
     'module:@youi/babel-plugin-react-native-youi',
+    // ['@babel/plugin-transform-typescript', { allowDeclareFields: true }],
   ],
   presets: ['module:metro-react-native-babel-preset'],
 };
