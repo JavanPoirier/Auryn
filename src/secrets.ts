1cab188bb309ac1e3c7b5bd50cd8ee61e2b2efd1
Erik Uggeldahl <erikuggeldahl@gmail.com>
2020-08-20 11:13:47 -0700

    WIP

--- src/secrets.ts
+++ src/secrets.ts
@@ -1,5 +1,5 @@
 // The TMDB API key can be obtained for free from:
 // https://developers.themoviedb.org/3/getting-started/introduction
-const tmdbApiKey = '';
+const tmdbApiKey = '7f5e61b6cef8643d2442344b45842192';
 
 export { tmdbApiKey };
